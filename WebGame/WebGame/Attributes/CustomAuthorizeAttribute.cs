69dc9438cc6ec2b8d8b18b6c987a8adbdaaf2173
andrey <Andrey_ishuk@mail.ru>
2022-08-22 13:09:23 +0300

    AuthenticationFilter

--- WebGame/WebGame/Attributes/CustomAuthorizeAttribute.cs
+++ WebGame/WebGame/Attributes/CustomAuthorizeAttribute.cs
@@ -2,8 +2,12 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using WebGame.Database.Model;
 
 namespace WebGame.Api.Attributes
@@ -19,9 +23,36 @@ namespace WebGame.Api.Attributes
                 return;
 
             // authorization
-            var user = (User)context.HttpContext.Items["User"];
-            if (user == null)
+            var token = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+
+            if (ValidateToken(token).IsFaulted || token == null)
+            {
                 context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+
+        private Task ValidateToken(string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("MySuperSecret_SecretKey123");
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+            }
+            catch
+            {
+                return Task.FromException(new TaskCanceledException());
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
