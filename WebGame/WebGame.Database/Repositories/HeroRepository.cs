99f5076195235ee598ae40f59279444811465ffd
Andrey <Andrey_ishuk@mail.ru>
2022-09-05 10:11:04 +0300

    Sql

--- WebGame/WebGame.Database/Repositories/HeroRepository.cs
+++ WebGame/WebGame.Database/Repositories/HeroRepository.cs
@@ -1,4 +1,6 @@
-﻿using WebGame.Database.Model;
+﻿using Npgsql;
+using System.Data;
+using WebGame.Database.Model;
 using WebGame.Database.Repositories.Interfaces;
 
 namespace WebGame.Database.Repositories
@@ -8,6 +10,31 @@ namespace WebGame.Database.Repositories
         public HeroRepository(WebGameDBContext context) : base(context)
         {
 
+        }
+
+        public DataSet GetAllSql()
+        {
+            var cs = "User ID=postgres; Password=postgres;Host=localhost;Port=5432;Database=WebGameBD;Pooling=true";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+            using var command = new NpgsqlCommand();
+
+            command.Connection = con;
+
+            command.CommandText = $"SELECT * FROM public.\"Heroes\"" /*WHERE public.\"Heroes\".\"Id\" = @id"*/;
+
+            //cmd.Parameters.AddWithValue("@id", heroId);
+
+            command.Prepare();
+
+            DataSet dataSet = new DataSet();
+
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(command);
+
+            dataAdapter.Fill(dataSet);
+
+            return dataSet;
         }
     }
 }
